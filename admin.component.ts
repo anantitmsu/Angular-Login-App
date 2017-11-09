10101eac90fe85943a8d483dd393cd293ec0d1c2
Anant Goswami <anant.itmsu@gmail.com>
2017-11-09 17:12:59 +0530

    Add files via upload

---
+++ admin.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Globals  } from './global';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+
+export class AdminComponent {
+
+
+}
